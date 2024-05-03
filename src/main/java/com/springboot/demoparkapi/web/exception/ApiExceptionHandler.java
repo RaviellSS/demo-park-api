664ae248800061771d95acfbcf32195fe738abf8
Raviell <raviell@msn.com>
2024-05-03 11:27:30 -0300

    Atualização do método de edição de senha em UsuarioService

--- src/main/java/com/springboot/demoparkapi/web/exception/ApiExceptionHandler.java
+++ src/main/java/com/springboot/demoparkapi/web/exception/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.springboot.demoparkapi.web.exception;
 
 import com.springboot.demoparkapi.exception.EntityNotFoundException;
+import com.springboot.demoparkapi.exception.PasswordInvalidException;
 import com.springboot.demoparkapi.exception.UsernameUniqueViolationException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler {
+
+    @ExceptionHandler(PasswordInvalidException.class)
+    public ResponseEntity<ErrorMessage> passwordInvalidException(RuntimeException ex,
+                                                                HttpServletRequest request){
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, ex.getMessage()));
+    }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorMessage> entityNotFoundException(RuntimeException ex,
