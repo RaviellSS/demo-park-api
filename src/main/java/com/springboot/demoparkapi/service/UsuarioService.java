2db3fcae39579055da8282a6ecd1f2647e73ee03
Raviell <raviell@msn.com>
2024-06-21 16:21:14 -0300

    Seção 9 - Camada de Testes

--- src/main/java/com/springboot/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/springboot/demoparkapi/service/UsuarioService.java
@@ -2,6 +2,7 @@ package com.springboot.demoparkapi.service;
 
 import com.springboot.demoparkapi.entity.Usuario;
 import com.springboot.demoparkapi.exception.EntityNotFoundException;
+import com.springboot.demoparkapi.exception.PasswordInvalidException;
 import com.springboot.demoparkapi.exception.UsernameUniqueViolationException;
 import com.springboot.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
@@ -32,14 +33,18 @@ public class UsuarioService {
         );
     }
 
-    @Transactional public Usuario editarSenha(Long id, String senhaAtual, String novaSenha, String confirmaSenha) {
-        if(!novaSenha.equals(confirmaSenha)){
-            throw new RuntimeException("Nova senha não confere com a confirmação de senha.");
+
+    @Transactional
+    public Usuario editarSenha(Long id, String senhaAtual, String novaSenha, String confirmaSenha) {
+        if (!novaSenha.equals(confirmaSenha)) {
+            throw new PasswordInvalidException("Nova senha não confere com confirmação de senha.");
         }
-        Usuario user = buscarPorId((id));
-        if(!user.getPassword().equals(senhaAtual)){
-            throw new RuntimeException("Sua senha não confere.");
+
+        Usuario user = buscarPorId(id);
+        if (!user.getPassword().equals(senhaAtual)) {
+            throw new PasswordInvalidException("Sua senha não confere.");
         }
+
         user.setPassword(novaSenha);
         return user;
     }
